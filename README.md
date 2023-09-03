685e0b18239d37498c790532358a0fbcbd5ae4ed
Snap <XxSnapperGeexX@gmail.com>
2023-09-03 16:32:04 -0700

    Add doc comments to day-hour-slot module and delete info from README that's repeated in module doc comments.

--- README.md
+++ README.md
@@ -20,53 +20,5 @@ Below shows 3 rows and the 3 separate components that make up each row:
 
 ![Day planner rows][day planner rows]
 
-### `DayHourSlot` constructor
-
-A `number` ranging from `0` to `23` represents the "first" hour of the day (12am) all the way to the "last" hour fo the
-day (11pm). Providing a value less than 0 or greater than 23, a non-integer value, or `undefined`/`null` causes an error
-to be thrown.
-
-```typescript
-// Creates a day hour slot for the first hour of the day, 12am
-const midnight = new DayHourSlot(0);
-
-// Creates a day hour slot for 4pm
-const fourPM = new DayHourSlot(16);
-
-// Creates a day hour slot for the last hour of the day, 11pm
-const midnight = new DayHourSlot(23);
-
-// All the following statements result in an error to be thrown:
-const noUndefined = new DayHourSlot(undefined);
-const noNull = new DayHourSlot(null);
-const noLessThan0 = new DayHourSlot(-1);
-const noGreaterThan23 = new DayHourSlot(42);
-const noNonInteger = new DayHourSlot(7.77);
-```
-
-### `DayHourSlot` properties
-
-`DayHourSlot` objects contain 6 properties and 1 method.
-
-- `hour`
-
-The `hour` property is the hour slot of a 24 hour day they represent. For instance, midnight/12am, is the hour `0` and
-4pm is the hour `16`.
-
-- `id`
-
-The `id` property is the value used as their id attribute/property in the DOM. This consists a string that is their
-`hour` prepended with `"hour-"`. So the `id` for 12am/midnight would be the string `"hour-0"` and the `id` for 4pm would
-be `"hour-16"`.
-
-- `$row`
-
-The `$row` property contains the JQuery `HTMLElement` object that is the root parent bootstrap row that encapsulates all
-the other JQuery `HTMLElement`s to be rendered to the viewport.
-
-- `$timeOfDay`
-
-The `$timeOfDay` property contains the 12 hour based time
-
 [day hour slot module]: ./src/main/ts/day-hour-slot.ts "day-hour-slot module"
 [day planner rows]: ./docs/day_planner_rows.png "Day planner rows breakdown"
--- src/main/ts/day-hour-slot.ts
+++ src/main/ts/day-hour-slot.ts
@@ -1,3 +1,11 @@
+/**
+ * This module contains the {@link DayHourSlot} class responsible for making objects used to create the hour rows
+ * rendered to the viewport. This module also exports some other components this class uses internally and can aid with
+ * interacting with it.
+ *
+ * @module day-hour-slot
+ */
+
 import $ from "jquery";
 
 const hourSlotRowClasses = Object.freeze(["row", "time-block", "past"]);
@@ -9,13 +17,47 @@ const saveButtonClasses = Object.freeze(["btn", "saveBtn", "col-2", "col-md-1"])
 const hourToTimeOfDay = (aNum: number): number => aNum === 0 || aNum === 12 ? 12 : aNum % 12;
 const hourToTimeOfDayString = (aNum: number): string => `${hourToTimeOfDay(aNum)}${aNum < 12 ? "AM" : "PM"}`;
 
+/**
+ * Contains `string`s for the 3 temporal times `"past"`, `"present"`, and `"future"`. These values are used to set
+ * whether a {@link DayHourSlot} object is in the past, present, or future relative to the current hour.
+ */
 export const temporalTimeValues: readonly string[] = Object.freeze(["past", "present", "future"]);
 
+/**
+ * Contains an object that can be used to access the 3 temporal times `"past"`, `"present"`, and `"future"` stored in
+ * the {@link temporalTimeValues} array. These values are used to set whether a {@link DayHourSlot} object is in the
+ * past, present, or future relative to the current hour.
+ */
 export const temporalTime: {readonly [tempTime in typeof temporalTimeValues[number] as Uppercase<tempTime>]: tempTime} = Object.freeze(
     Object.fromEntries(
         Array.from(temporalTimeValues.values()).map(tempTime => [tempTime.toUpperCase(), tempTime])
 ));
 
+/**
+ * This class is used to create objects for the hour rows rendered to the viewport. A singe integer `number` argument
+ * is used to dictate what hour of the day the rendered row is to represent. For instance, passing `0` results in a row
+ * being created for the first hour of the day (12am/midnight) and `23` results in a row being created for last hour of
+ * the day (11pm).
+ *
+ * @example
+ * ```typescript
+ * // Creates a day hour slot for the first hour of the day, 12am
+ * const midnight = new DayHourSlot(0);
+ *
+ * // Creates a day hour slot for 4pm
+ * const fourPM = new DayHourSlot(16);
+ *
+ * // Creates a day hour slot for the last hour of the day, 11pm
+ * const midnight = new DayHourSlot(23);
+ *
+ * // All the following statements result in an error to be thrown:
+ * const noUndefined = new DayHourSlot(undefined);
+ * const noNull = new DayHourSlot(null);
+ * const noLessThan0 = new DayHourSlot(-1);
+ * const noGreaterThan23 = new DayHourSlot(42);
+ * const noNonInteger = new DayHourSlot(7.77);
+ * ```
+ */
 export class DayHourSlot
 {
     readonly #hour: number;
@@ -25,6 +67,16 @@ export class DayHourSlot
     readonly #$textArea: JQuery<HTMLElement>;
     readonly #$saveButton: JQuery<HTMLElement>;
 
+    /**
+     * Constructs an object instance used to create the hour rows rendered to the viewport. A singe integer `number`
+     * argument is used to dictate what hour of the day the rendered row is to represent.
+     *
+     * @param hourNum A `number` ranging from `0` to `23` to set which our of the day the constructed object is for.
+     *
+     * @throws {@link TypeError} if the passed `hourNum` argument is not an integer.
+     *
+     * @throws {@link RangeError} if the passed `hourNum` argument is not inclusively between `0` and `23`.
+     */
     public constructor(hourNum: NonNullable<number>)
     {
         if (typeof hourNum !== "number")
@@ -72,13 +124,62 @@ export class DayHourSlot
                                .append(this.#$timeOfDay, $textAreaLabel, this.#$textArea, this.#$saveButton);
     }
 
+    /**
+     * Which hour of a 24 hour day this {@link DayHourSlot} represents. For instance, midnight/12am, is the hour `0` and
+     * 4pm is the hour `16`.
+     */
     public get hour(): number { return this.#hour; }
+
+    /**
+     * The attribute/property used to identify this {@link DayHourSlot} in the DOM. This consists of a `string` that is
+     * the {@link DayHourSlot.hour hour} prepended with `"hour-"`. So the `id` for 12am/midnight would be the string
+     * `"hour-0"` and the `id` for 4pm would be `"hour-16"`.
+     */
     public get id(): string { return this.#id; }
+
+    /**
+     * The root JQuery `HTMLElement` bootstrap row to be rendered to the viewport. This is the root parent that contains
+     * all the other JQuery `HTMLElement` components of this {@link DayHourSlot} object found in its properties:
+     *
+     * - {@link DayHourSlot.$timeOfDay $timeOfDay}
+     * - {@link DayHourSlot.$textArea $textArea}
+     * - {@link DayHourSlot.$saveButton $saveButton}
+     */
     public get $row(): JQuery<HTMLElement> { return this.#$row; }
+
+    /**
+     * The JQuery `HTMLElement` used to render the time of day tab the left of the text input area denoting which hour
+     * of the day the hour row is for. For instance, if this {@link DayHourSlot} object is for hour `14`, the time of
+     * day will contain the text `"12PM"`.
+     */
     public get $timeOfDay(): JQuery<HTMLElement> { return this.#$timeOfDay; }
+
+    /**
+     * The JQuery `HTMLElement` used to render the text area input to save the text associated with this hour of the day
+     * that gets saved to local storage when the save button is clicked.
+     */
     public get $textArea(): JQuery<HTMLElement> { return this.#$textArea; }
+
+    /**
+     * The JQuery `HTMLElement` used to render the save button to the right of the text area input that saves the
+     * contents of the text area into local storage.
+     */
     public get $saveButton(): JQuery<HTMLElement> { return this.#$saveButton; }
 
+    /**
+     * This method adds the class `past`, `present`, or `future` to this {@link DayHourSlot} object's
+     * {@link DayHourSlot.$row $row} which dictates the background color of its text input area that gets rendered to
+     * the viewport.
+     *
+     * @param temporalTime Value indicating whether the hour this {@link DayHourSlot} object is for is in the past,
+     *                     present, or future. It can be a `string` with the values `"past"`, `"present"`, or `"future"`
+     *                     or a `number` where any negative value is considered past, `0` is considered present, and any
+     *                     value greater than `0` is considered future.
+     *
+     * @returns this {@link DayHourSlot} object instance.
+     *
+     * @throws {@link TypeError} if the passed argument isn't a `string` or `number`.
+     */
     public setTemporalTime(temporalTime: NonNullable<string | number>): DayHourSlot
     {
         if (typeof temporalTime !== "string" && typeof temporalTime !== "number")
@@ -94,7 +195,7 @@ export class DayHourSlot
         {
             if ( ! temporalTimeValues.includes(temporalTime))
             {
-                throw new Error(`${this.constructor.name}: ${arguments.callee.name}: argument must be either ${temporalTimeValues.map(t => `"${t}"`).join(", ")}: "${temporalTime}"`);
+                throw new Error(`${this.constructor.name}: ${arguments.callee.name}: argument must be ${temporalTimeValues.map(t => `"${t}"`).join(", ")}: "${temporalTime}"`);
             }
 
             this.#$row.removeClass(Array.from(temporalTimeValues)).addClass(temporalTime);
