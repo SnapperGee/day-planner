cfdf4e35d9cd1ec4d6329abb322de297e9891e18
Snap <XxSnapperGeexX@gmail.com>
2023-09-04 11:16:55 -0700

    Make webpack config files us esm and move into config directory.

---
+++ config/dist.webpack.config.mjs
@@ -0,0 +1,36 @@
+import { resolve as resolvePath, dirname } from "node:path";
+import { fileURLToPath } from 'url';
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default {
+    entry: [resolvePath('.', 'src', 'main', 'ts', 'script.ts'), resolvePath('.', 'src', 'main', 'scss', 'index.scss')],
+    module: {
+    rules: [
+        {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+        },
+        {
+            test: /\.s[a|c]ss$/,
+            use: [MiniCssExtractPlugin.loader,'css-loader', 'sass-loader']
+        }
+    ],
+    },
+    resolve: { extensions: ['.tsx', '.ts', '.js'], },
+    output: {
+        filename: 'index.min.mjs',
+        path: resolvePath(__dirname, '..', 'build'),
+    },
+    plugins: [
+        new MiniCssExtractPlugin({ filename: "index.min.css" }),
+        new HtmlWebpackPlugin({
+            template: resolvePath('.', 'src', 'main', 'html', 'index.html')
+        }),
+        new webpack.ProvidePlugin({ $: "jquery", jQuery: "jquery" })
+    ]
+};
