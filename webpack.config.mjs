937e8de1460ce1620d209ca8b5bc32c083ba0d69
Snap <XxSnapperGeexX@gmail.com>
2023-09-04 11:48:05 -0700

    Create single root webpack config that dynamically sets config options and delete config directory.

---
+++ webpack.config.mjs
@@ -0,0 +1,36 @@
+import { resolve as resolvePath } from "node:path";
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+export default function(env, argv) {
+    return {
+        entry: [resolvePath('.', 'src', 'main', 'ts', 'script.ts'), resolvePath('.', 'src', 'main', 'scss', 'index.scss')],
+        mode: env.production ? 'production' : 'development',
+        module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s[a|c]ss$/,
+                use: [env.production ? MiniCssExtractPlugin.loader : 'style-loader', 'css-loader', 'sass-loader']
+            }
+        ],
+        },
+        resolve: { extensions: ['.tsx', '.ts', '.js'], },
+        output: {
+            filename: env.production ? 'index.min.mjs' : 'index.mjs',
+            path: resolvePath('.', 'build'),
+        },
+        plugins: [
+            new MiniCssExtractPlugin({ filename: "index.min.css" }),
+            new HtmlWebpackPlugin({
+                template: resolvePath('.', 'src', 'main', 'html', 'index.html')
+            }),
+            new webpack.ProvidePlugin({ $: "jquery", jQuery: "jquery" })
+        ]
+    }
+};
