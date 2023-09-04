1b682c648317dfd40930c79638b84c55a6e5c66f
Snap <XxSnapperGeexX@gmail.com>
2023-09-04 02:49:03 -0700

    Use webpack to build/load html and scss.

--- dev.webpack.config.cjs
+++ dev.webpack.config.cjs
@@ -1,8 +1,8 @@
 const resolvePath = require('path').resolve;
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: resolvePath('.', 'src', 'main', 'ts', 'script.ts'),
+module.exports = {entry: [resolvePath('.', 'src', 'main', 'ts', 'script.ts'), resolvePath('.', 'src', 'main', 'scss', 'index.scss')],
   module: {
     rules: [
       {
@@ -10,16 +10,23 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.s[a|c]ss$/,
+        use: ['style-loader','css-loader', 'sass-loader']
+     }
     ],
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
-    filename: 'index.min.mjs',
+    filename: 'index.mjs',
     path: resolvePath(__dirname, 'build'),
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: resolvePath('.', 'src', 'main', 'html', 'index.html')
+    }),
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery"
