f6ca32673db71adad0df69ec5303ba25180bb7a1
Snap <XxSnapperGeexX@gmail.com>
2023-09-05 01:44:26 -0700

    Add doc comments to create-hours-array module.

--- src/main/ts/create-hours-array.ts
+++ src/main/ts/create-hours-array.ts
@@ -1,3 +1,19 @@
+/**
+ * @module create-hours-array
+ */
+
+/**
+ * Takes in an array of numbers and returns a new array based on the passed array formatted to be a valid number range
+ * that can be used to create an array of {@link day-hour-slot.DayHourSlot} objects.
+ *
+ * @param numArray Array of integers to validate and sort.
+ *
+ * @returns A sorted of array of distinct numbers.
+ *
+ * @throws {@link RangeError} if any value in the passed array is less than `0` or greater than `23`.
+ *
+ * @throws {@link Error} if passed array contains non distinct values.
+ */
 export const createHoursArray = (numArray: number[]): number[] =>
 {
     if (numArray.length < 2)
@@ -19,14 +35,12 @@ export const createHoursArray = (numArray: number[]): number[] =>
         throw new RangeError(`${createHoursArray.name}: max greater 23: ${min}`);
     }
 
-    const numSet: ReadonlySet<number> = Object.freeze(new Set(numArray));
-
-    if (numSet.size !== numArray.length)
+    if (numArray.some(aNum => numArray.indexOf(aNum) !== numArray.lastIndexOf(aNum)))
     {
         throw new Error(`${createHoursArray.name}: duplicate values in num array: ${numArray}`);
     }
 
-    return Array.from(numSet).sort((aNum, anotherNum) => aNum - anotherNum);
+    return Array.from(numArray).sort((aNum, anotherNum) => aNum - anotherNum);
 }
 
 export default createHoursArray;
